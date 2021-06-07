cb78f13a2cf01b9a9161d4e4de6d327657ddfaa7
sc-yz <wanganqing0502@163.com>
2021-06-07 03:25:10 +0000

    Deploying to gh-pages-test from  @ 2e0b4ee53064bce2ac10cf17a74219be031f05c1 🚀

---
+++ precache-manifest.40c5158fba3bf190f0030ffaf04e4b26.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5cb93974d49bb3b532f9",
+    "url": "/vue-blog/css/app.d32076ef.css"
+  },
+  {
+    "revision": "5f511a3cf71d76543de4",
+    "url": "/vue-blog/css/chunk-vendors.a74d279d.css"
+  },
+  {
+    "revision": "44401ffa59d3207e2cb9",
+    "url": "/vue-blog/css/index.d572f15f.css"
+  },
+  {
+    "revision": "85f027895782a31a5f696e3dfb32a2e0",
+    "url": "/vue-blog/index.html"
+  },
+  {
+    "revision": "5cb93974d49bb3b532f9",
+    "url": "/vue-blog/js/app.3bab02b8.js"
+  },
+  {
+    "revision": "b31823ee5c0d1809192b",
+    "url": "/vue-blog/js/chunk-2d0b21a7.4ce186a5.js"
+  },
+  {
+    "revision": "a40cfe0d71b1b4c6d358",
+    "url": "/vue-blog/js/chunk-2d0bac1b.afb7f591.js"
+  },
+  {
+    "revision": "5bcf5bcd6e3ce5ce389b",
+    "url": "/vue-blog/js/chunk-2d207375.6f4cd59b.js"
+  },
+  {
+    "revision": "7ea9845db81c27a361f3",
+    "url": "/vue-blog/js/chunk-2d20f528.59254eaf.js"
+  },
+  {
+    "revision": "742d089c6ea3bf15f18b",
+    "url": "/vue-blog/js/chunk-2d216461.b845f7e0.js"
+  },
+  {
+    "revision": "4642ffff1899d5190f26",
+    "url": "/vue-blog/js/chunk-2d21dbc7.5b71df64.js"
+  },
+  {
+    "revision": "5f511a3cf71d76543de4",
+    "url": "/vue-blog/js/chunk-vendors.aa31c916.js"
+  },
+  {
+    "revision": "44401ffa59d3207e2cb9",
+    "url": "/vue-blog/js/index.2c393c55.js"
+  },
+  {
+    "revision": "1a73d38642f2251764fd",
+    "url": "/vue-blog/js/post-browser-cache.48218bfd.js"
+  },
+  {
+    "revision": "2c76446a58ea732ed4fd",
+    "url": "/vue-blog/js/post-burying-point.22fa875d.js"
+  },
+  {
+    "revision": "6c6d813a99d95ab7653a",
+    "url": "/vue-blog/js/post-eslint-prettier.1ef13957.js"
+  },
+  {
+    "revision": "d6b500f7f5cb05a10f49",
+    "url": "/vue-blog/js/post-frontend-knowledge.5b5acfc2.js"
+  },
+  {
+    "revision": "c18e27e21c6b382347e4",
+    "url": "/vue-blog/js/post-service-worker.a2723854.js"
+  },
+  {
+    "revision": "5458259741428053c14b",
+    "url": "/vue-blog/js/post-use-markdown.b18d1a3f.js"
+  },
+  {
+    "revision": "67060cf72485dfb91767",
+    "url": "/vue-blog/js/post-what-happens-when-you-type-an-url-in-the-browser-and-press-enter.441c706d.js"
+  },
+  {
+    "revision": "bc9a2d24b88e57f80dc8949bab6da922",
+    "url": "/vue-blog/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-blog/robots.txt"
+  },
+  {
+    "revision": "2af6b331a1d55953159a6845ce8979a6",
+    "url": "/vue-blog/sw.js"
+  }
+]);
---
+++ sw.js
@@ -0,0 +1,48 @@
+importScripts("https://g.alicdn.com/kg/workbox/3.3.0/workbox-sw.js", "/vue-blog/precache-manifest.40c5158fba3bf190f0030ffaf04e4b26.js");
+
+// Note: Ignore the error that Glitch raises about workbox being undefined.
+
+// importScripts('https://g.alicdn.com/kg/workbox/3.3.0/workbox-sw.js');
+
+workbox.setConfig({
+  modulePathPrefix: 'https://g.alicdn.com/kg/workbox/3.3.0/',
+});
+if (workbox) {
+  console.log('Yay! Workbox is loaded');
+} else {
+  console.log('Boo! Workbox did not load');
+}
+workbox.routing.registerRoute(
+  // Cache CSS files
+  /.*\.css/,
+  // 使用缓存，但尽快在后台更新
+  workbox.strategies.staleWhileRevalidate({
+    // 使用自定义缓存名称
+    cacheName: 'css-cache',
+  })
+);
+workbox.routing.registerRoute(
+  // 缓存JS文件
+  /.*\.js/,
+  // 使用缓存，但尽快在后台更新
+  workbox.strategies.staleWhileRevalidate({
+    // 使用自定义缓存名称
+    cacheName: 'js-cache',
+  })
+);
+workbox.routing.registerRoute(
+  // 缓存gravatar文件
+  new RegExp('https://cdn.v2ex.com/'),
+  // 如果缓存可用，请使用它
+  workbox.strategies.cacheFirst({
+    // 使用自定义缓存名称
+    cacheName: 'gravatar-cache',
+    plugins: [
+      new workbox.expiration.Plugin({
+        // 缓存最多30天
+        maxAgeSeconds: 30 * 24 * 60 * 60,
+      }),
+    ],
+  })
+);
+
