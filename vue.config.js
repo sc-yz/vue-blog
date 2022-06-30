f76da1c7738a0ce84a3141a016e35cac51dfb4e1
wanganqing <905330374@qq.com>
2022-07-01 00:17:28 +0800

    fix: publicPath

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,7 @@ const CopyWebpackPlugin = require('copy-webpack-plugin');
 const path = require('path');
 
 module.exports = {
-  publicPath: '/vue-blog',
+  publicPath: '',
   lintOnSave: process.env.NODE_ENV !== 'production',
   chainWebpack: (config) => {
     config.module
