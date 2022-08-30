b70a5bb44343f384a758ddc34645fb56dcc35d2b
wanganqing <wanganqing0502@163.com>
2022-08-30 16:28:50 +0800

    fix: 语音

--- src/pages/about/index.vue
+++ src/pages/about/index.vue
@@ -2,7 +2,7 @@
  * @Author: wanganqing wanganqing0502@163.com
  * @Date: 2021-07-09 14:03:43
  * @LastEditors: wanganqing wanganqing0502@163.com
- * @LastEditTime: 2022-08-30 16:16:57
+ * @LastEditTime: 2022-08-30 16:28:01
  * @FilePath: /vue-blog-github/src/pages/about/index.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
@@ -47,7 +47,7 @@ export default {
     stopRecorder() {
       this.showSound = false;
       this.recorder.stop();
-      this.recorder.destroy().then(function () {});
+      // this.recorder.destroy().then(function () {});
     },
     // 录音播放
     playRecorder() {
