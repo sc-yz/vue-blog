f8a2c2456df0d73ffe3608be441c2ca5a480912f
wanganqing <wanganqing0502@163.com>
2022-09-02 14:40:55 +0800

    feat: 初始化开始录音

--- src/pages/about/index.vue
+++ src/pages/about/index.vue
@@ -2,7 +2,7 @@
  * @Author: wanganqing wanganqing0502@163.com
  * @Date: 2021-07-09 14:03:43
  * @LastEditors: wanganqing wanganqing0502@163.com
- * @LastEditTime: 2022-09-02 14:35:31
+ * @LastEditTime: 2022-09-02 14:40:52
  * @FilePath: /vue-blog-github/src/pages/about/index.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
@@ -179,7 +179,7 @@ export default {
       });
     },
     perssion() {
-      navigator.getUserMedia(
+      navigator.mediaDevices.getUserMedia(
         { audio: true },
         function onSuccess() {
           console.log('已允许，开启成功');
